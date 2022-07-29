f880b4366db93157b431d81f71b9ea5ee7e8901a
Laurentiu Niculae <niculae.laurentiu1@gmail.com>
2022-07-29 13:08:44 +0300

    boltdb query logic
    (cherry picked from commit e3cb60b8566d84158409815325083ed149175063)
    
    Some other fixes/improvements on top (Andrei)
    
    Global search: The last updated attribute on repo level is now computed correctly.
    Global search: Fix and enhance tests: validate more fields, and fix CVE verification logic
    RepoListWithNewestImage: The vendors and platforms at repo level are no longer containing duplicate entries
    CVE: scan OCIUncompressedLayer instead of skiping them (used in tests)
    
    Add filtering to global search API
    
    (cherry picked from commit a87976d635ea876fe8ced532e8adb7c3bb24098f)
    
    Original work by Laurentiu Niculae <niculae.laurentiu1@gmail.com>

--- pkg/extensions/search/common/oci_layout.go
+++ pkg/extensions/search/common/oci_layout.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"path"
 	"strconv"
+	"strings"
 	"time"
 
 	v1 "github.com/google/go-containerregistry/pkg/v1"
@@ -343,8 +344,8 @@ func (olu BaseOciLayoutUtils) GetExpandedRepoInfo(name string) (RepoInfo, error)
 		return RepoInfo{}, err
 	}
 
-	repoPlatforms := make([]OsArch, 0)
-	repoVendors := make([]string, 0, len(manifestList))
+	repoVendorsSet := make(map[string]bool, len(manifestList))
+	repoPlatformsSet := make(map[string]OsArch, len(manifestList))
 
 	var lastUpdatedImageSummary ImageSummary
 
@@ -381,13 +382,16 @@ func (olu BaseOciLayoutUtils) GetExpandedRepoInfo(name string) (RepoInfo, error)
 			continue
 		}
 
-		os, arch := olu.GetImagePlatform(imageConfigInfo)
+		opSys, arch := olu.GetImagePlatform(imageConfigInfo)
 		osArch := OsArch{
-			Os:   os,
+			Os:   opSys,
 			Arch: arch,
 		}
 
-		repoPlatforms = append(repoPlatforms, osArch)
+		if opSys != "" || arch != "" {
+			osArchString := strings.TrimSpace(fmt.Sprintf("%s %s", opSys, arch))
+			repoPlatformsSet[osArchString] = osArch
+		}
 
 		layers := make([]LayerSummary, 0)
 
@@ -410,7 +414,9 @@ func (olu BaseOciLayoutUtils) GetExpandedRepoInfo(name string) (RepoInfo, error)
 		// get image info from manifest annotation, if not found get from image config labels.
 		annotations := GetAnnotations(manifest.Annotations, imageConfigInfo.Config.Labels)
 
-		repoVendors = append(repoVendors, annotations.Vendor)
+		if annotations.Vendor != "" {
+			repoVendorsSet[annotations.Vendor] = true
+		}
 
 		size := strconv.Itoa(int(imageSize))
 		manifestDigest := man.Digest.Hex()
@@ -452,6 +458,19 @@ func (olu BaseOciLayoutUtils) GetExpandedRepoInfo(name string) (RepoInfo, error)
 	}
 
 	size := strconv.FormatInt(repoSize, 10)
+
+	repoPlatforms := make([]OsArch, 0, len(repoPlatformsSet))
+
+	for _, osArch := range repoPlatformsSet {
+		repoPlatforms = append(repoPlatforms, osArch)
+	}
+
+	repoVendors := make([]string, 0, len(repoVendorsSet))
+
+	for vendor := range repoVendorsSet {
+		vendor := vendor
+		repoVendors = append(repoVendors, vendor)
+	}
 
 	summary := RepoSummary{
 		Name:        name,
